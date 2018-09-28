7742cda6488faa0aea3ebcfe897479911cbbfb45
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-09-28 14:30:58 -0400

    did some changes

--- DrawingShapesKatheryn/main.lua
+++ DrawingShapesKatheryn/main.lua
@@ -1,10 +1,12 @@
 -----------------------------------------------------------------------------------------
--- Nmae :Katheryn B.M
+-- Name :Katheryn B.M
 -- Course: ICS20
 -- This program will make shapes with colour. The background will also be coloured.
 -----------------------------------------------------------------------------------------
 
 --create my local variables for my polygon
-local myTriHalfW = display.contentWidth ( * 1 / 4)
-local myTriHalfH = display.contentHight ( * 1 / 4)
-local verticesofTriangle = { 500,370, 300,-450, 50,500, 600,1024, }
+local halfWTri = display.contentWidth * 0.2
+local halfHTri = display.contentHeight * 0.2
+local verticesTriangle = { 500,370, 150,-225, 25,250, 300,512 }
+local triangle
+local myTriangle = display.newPolygon( halfWTri,halfHTri, verticesTriangle )

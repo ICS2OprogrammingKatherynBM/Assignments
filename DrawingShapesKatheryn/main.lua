9bff5f96695f92acc86790ba0dc299cd299a655b
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-09-28 14:10:37 -0400

    Modifed main.lua

--- DrawingShapesKatheryn/main.lua
+++ DrawingShapesKatheryn/main.lua
@@ -1,7 +1,10 @@
 -----------------------------------------------------------------------------------------
 -- Nmae :Katheryn B.M
--- main.lua
---
+-- Course: ICS20
+-- This program will make shapes with colour. The background will also be coloured.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--create my local variables for my polygon
+local myTriHalfW = display.contentWidth ( * 1 / 4)
+local myTriHalfH = display.contentHight ( * 1 / 4)
+local verticesofTriangle = { 500,370, 300,-450, 50,500, 600,1024, }

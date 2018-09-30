c1b5727bcc4be545f0b79afc6b0d8bb25048b88f
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-09-30 16:34:48 -0400

    finished a couple of finishing touches

--- DrawingShapesKatheryn/main.lua
+++ DrawingShapesKatheryn/main.lua
@@ -1,12 +1,54 @@
 -----------------------------------------------------------------------------------------
+ --Title: Drawing Shapes
 -- Name :Katheryn B.M
 -- Course: ICS20
 -- This program will make shapes with colour. The background will also be coloured.
 -----------------------------------------------------------------------------------------
 
---create my local variables for my polygon
-local halfWTri = display.contentWidth * 0.2
-local halfHTri = display.contentHeight * 0.2
-local verticesTriangle = { 500,370, 150,-225, 25,250, 300,512 }
-local triangle
-local myTriangle = display.newPolygon( halfWTri,halfHTri, verticesTriangle )
+--Set background color
+display.setDefault("background", 229/255, 204/255,255/255 )
+
+-- Hide status bar 
+display.setStatusBar(display.HiddenStatusBar ) 
+
+--Make rounded rectangle
+local roundedRect = display.newRoundedRect( 185, 165, 275, 154, 35 )
+roundedRect.path.radius = 150
+
+--Set color for rounded rectangle
+roundedRect:setFillColor( 0/255, 127/255, 127/255  )
+
+--Make a polygon
+local myPolygon
+local polygonVertices = {10,-90, 80,-25, 33,0, 40,80, 10,35 }
+myPolygon = display.newPolygon(520, 292, polygonVertices )
+
+--Set color for Polygon
+myPolygon.fill = { 0/255, 127/255, 127/255 }
+ 
+ -- Make a Triangle
+ local myTriangle
+ local triangleVertices = { 75,50, 25,200, 130,200 }
+ local myTriangle = display.newPolygon(300,550, triangleVertices ) 
+
+ --Set color for Triangle 
+ myTriangle.fill = { 0/255, 127/255, 127/255 }
+
+--Set text for rounded Rectangle
+textObject = display.newText("Rounded Rectangle", 215, 330, native.systemFont, 43 )
+
+--Set color of text
+textObject:setFillColor( 204/255, 0/255, 102/255 )
+
+--Set text for Polygon
+textObject = display.newText(" Random Polygon", 635, 175, native.systemFont, 43 )
+
+--set color for text
+textObject:setFillColor( 204/255, 0/255, 102/255)
+
+--Set text for Triangle 
+textObject = display.newText(" Triangle", 300, 670, native.systemFont, 43 )
+
+--Set color for text
+textObject:setFillColor(204/255, 0/255, 102/255)
+

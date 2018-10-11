2dde96833391a594fced99cb4b9cb5a05e8c3985
katherynbohl-martinez <119+katherynbohl-martinez@users.noreply.github.com>
2018-10-11 17:12:18 -0400

    did some small changes

--- AnimatingImagesKatheryn/main.lua
+++ AnimatingImagesKatheryn/main.lua
@@ -8,22 +8,36 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 --global Variables
-scrollSpeed = 3
+scrollSpeed = 4
 
 --the background image with width and height 
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
 
---character images with width and height
+--characters images with width and height
 local ghost = display.newImageRect("Images/ghost.png", 300, 300)
+
+local butterfly =  display.newImageRect("Images/butterfly.png", 300, 300)
+
+local turtle = display.newImageRect("Images/turtle.png",300, 300)
+
+--set initial position of turtle
+turtle.x = 1
+turtle.y = display.contentHeight/2
+--set initial position of the butterfly
+butterfly.x = 1100
+butterfly.y = 100
 
 --set ghost to be transparent
 ghost.alpha = 0
+
+--set the transparency of butterfly
+butterfly.alpha = 1
 
 --set initial position of ghost
 ghost.x = display.contentWidth/1
 ghost.y = 3
 
---Function : MoveShip
+--Function : MoveGhost
 -- Input : This function accepts an event listener
 -- Output : none 
 --Discription : This function adds the scroll speed for the Ghost
@@ -32,10 +46,24 @@ local function MoveGhost(event)
 	ghost.y = ghost.x - scrollSpeed
 	ghost.x = ghost.y - scrollSpeed
 	--change the transparency of the ghost every time it moves so that it fades out
-	ghost.alpha = ghost.alpha + 0.001
+	ghost.alpha = ghost.alpha + 0.01
 end
 
 --MoveGhost will be called over and over again 
 Runtime:addEventListener("enterFrame",MoveGhost)
 
+
+--Function : Movebutterfly
+-- Input : This function accepts an event listener
+-- Output : none 
+--Discription : This function adds the scroll speed for the Butterfly
+local function MoveButterfly(event)
+	--add the scroll speed of the butterfly
+	butterfly.x = butterfly.x - scrollSpeed
+	--change the transparency of the ghost every time it moves so that it fades in
+	butterfly.alpha = butterfly.alpha - 0.001
+end
+
+--MoveGhost will be called over and over again 
+Runtime:addEventListener("enterFrame",MoveButterfly)
 

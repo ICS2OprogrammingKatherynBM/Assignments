32abfcd7f32d485d7a0b60f9800ff718161a9ee1
katherynbohl-martinez <katherynbohl-martinez@users.noreply.github.com>
2018-11-15 13:36:31 -0500

    added

--- CompanyLogoAnimationKatherynBM/main.lua
+++ CompanyLogoAnimationKatherynBM/main.lua
@@ -7,27 +7,7 @@
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
---load phisics
-local physics = require("physics")
 
---start phisics
-physics.start()
------------------------------------------------------------------------------------------
--- Objects
------------------------------------------------------------------------------------------
---ground
-local ground = display.newImage("Images/ground.png", 0, 0)
-
---put the ground at the bottom
-ground.x = display.contentWidth/2
-ground.y = display.contentHeight/1
-
--- change the wisth to be the same as the screen
-ground.width = display.contentWidth * 2
-
---add  to physics
-physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
------------------------------------------------------------------------------------------
 
 
 

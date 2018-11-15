cdd915bf502e64bfd8d10ce455fcf426d127bb98
katherynbohl-martinez <katherynbohl-martinez@users.noreply.github.com>
2018-11-15 14:18:05 -0500

    added stuff

--- CompanyLogoAnimationKatherynBM/main.lua
+++ CompanyLogoAnimationKatherynBM/main.lua
@@ -7,12 +7,31 @@
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
-<<<<<<< HEAD
-
-=======
 --background
 display.setDefault("background", 0/255, 160/255, 0/255)
->>>>>>> 96e54e16160cafbb11a2954c8238befdc4138985
+
+--global variable
+scrollspeed = 3
+--the character
+local pizzaMan = display.newImageRect("Images/CompanyLogoKatheryn@2x.png", 400, 400)
+
+--make pizza man transparent
+pizzaMan.alpha = 0
+
+--set the position for the pizza man
+pizzaMan.x = display.contentWidth/8
+pizzaMan.y = display.contentHeight/2
+
+--local function
+local function MovePizzaMan(event)
+	-- scroll speed
+	pizzaMan.x = pizzaMan.x + scrollspeed
+	--change the transparency
+	pizzaMan.alpha = pizzaMan.alpha + 0.01
+end
+
+--call function 
+Runtime:addEventListener("enterFrame", MovepizzaMan)
 
 
 
